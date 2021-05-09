3e20c7af6fb8cea2de3fbb92da3c974448941820
Snarux <39747726+Snarux@users.noreply.github.com>
2021-05-09 20:41:16 +0100

    Update client_vars.lua

--- client_vars.lua
+++ client_vars.lua
@@ -1,18 +1,21 @@
 positions = {
 	
-	--Prédio mais alto
+	-- Prédio mais alto
+	-- Tallest building
     {{-75.09, -824.94, 320.29}, 
 	{-79.39, -836.86, 39.55, 0},	
 	{0,0,0}, "1st Teleporte"}, 
 	
 	
 	-- Teleferico
+	-- Cable car
     {{-740.8442, 5596.4799, 40.6546}, 
 	{446.5397, 5580.4003, 780.1894, 0},
 	{0,0,0}, "2nd Teleporte"},
 	
 	
 	-- Prédio mais alto na saída da cidade
+	-- Tallest building just outside the city
     {{-755.0234, 334.6084, 229.6368}, 
 	{-784.3550, 352.3603, 86.9982, 0},
 	{0,0,0}, "3nd Teleporte"},
